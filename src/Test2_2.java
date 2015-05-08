a37b666a8c0d547cc7d7a06ef4a0eb94f77a0fc7
WuYan <wuyan1222@126.com>
2015-05-08 18:08:57 +0800

    First

---
+++ src/Test2_2.java
@@ -0,0 +1,10 @@
+public class Test2_2
+{
+    public static void main(String args[])
+    {
+        int x;
+        for (x = 1; x <10 ; x++) {
+            System.out.println(x + "*" + x + "=" + x * x);
+            System.out.println();
+    }
+}
