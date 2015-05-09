df3d27865ec87e941c1494862e6de5ccdec28025
WuYan <wuyan1222@126.com>
2015-05-09 17:24:45 +0800

    Member variable.

---
+++ src/TestMemVar.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yan on 2015/5/9.
+ */
+public class TestMemVar {
+
+    static int a = 1;
+    public static void main(String args[])
+    {
+        System.out.println("Member variable value is : "+a);
+    }
+}
