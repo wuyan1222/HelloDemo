011100ccfed0d52098f918ca230094682f700686
WuYan <wuyan1222@126.com>
2015-05-09 22:22:28 +0800

    LocalVariable

---
+++ src/LocalVariable.java
@@ -0,0 +1,13 @@
+/**
+ * Created by yan on 2015/5/9.
+ */
+public class LocalVariable {
+    public static void main(String args[]){
+        int sum = 0;
+        for (int i = 1; i <=5; i++) {
+            sum = sum +i;
+            System.out.println("i = "+i+" ,sum ="+sum);
+
+        }
+    }
+}
