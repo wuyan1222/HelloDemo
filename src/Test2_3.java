1b3cf215a4ac816c109ec26304a23bdf06221b9b
WuYan <wuyan1222@126.com>
2015-05-09 17:04:32 +0800

    So Easy.

---
+++ src/Test2_3.java
@@ -0,0 +1,18 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by yan on 2015/5/9.
+ */
+public class Test2_3
+{
+    public static void main(String args[])
+    {
+        int num1 = 3;
+        int num2 = 2;
+
+        System.out.println("I have "+num1+" books!");
+        System.out.println("You have "+num2+" books!");
+
+    }
+
+}
