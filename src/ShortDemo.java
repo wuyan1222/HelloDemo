ac47aa4a52ef4f2d489d3371f78a7a56cae70367
WuYan <wuyan1222@126.com>
2015-05-11 18:57:37 +0800

    ShortDemo

---
+++ src/ShortDemo.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yan on 2015/5/11.
+ */
+public class ShortDemo {
+    public static void main(String args[]){
+        short short_max = Short.MAX_VALUE;
+        System.out.println("Short Max Value = "+short_max);
+    }
+}
