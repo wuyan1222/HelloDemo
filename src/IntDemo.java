d98d124b92e817ceb7717b83e3881fe071d253b9
WuYan <wuyan1222@126.com>
2015-05-11 19:02:00 +0800

    IntDemo

---
+++ src/IntDemo.java
@@ -0,0 +1,10 @@
+/**
+ * Created by yan on 2015/5/11.
+ */
+public class IntDemo {
+    public static void main(String args[]) {
+        int int_max = Integer.MAX_VALUE;
+        System.out.println("Integar Max Value = " + int_max);
+    }
+}
+
