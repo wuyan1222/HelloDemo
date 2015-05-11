51f818c366ca6b3a954812d9f94895070903fb8d
WuYan <wuyan1222@126.com>
2015-05-11 19:08:10 +0800

    FloatDemo

---
+++ src/Test3_8.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yan on 2015/5/11.
+ */
+public class Test3_8 {
+    public static void main(String args[]){
+        float num = 4.2f;
+        System.out.println(num+"*"+num+"="+(num*num));
+    }
+}
