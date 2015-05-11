bbd181423306d48039f445cbd33102d4b21a0b8d
WuYan <wuyan1222@126.com>
2015-05-11 19:12:09 +0800

    DoubleDemo

---
+++ src/Test3_9.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yan on 2015/5/11.
+ */
+public class Test3_9 {
+    public static void main(String args[]){
+        System.out.println("float_max = "+Float.MAX_VALUE);
+        System.out.println("float_min = "+Float.MIN_VALUE);
+        System.out.println("double_max = "+Double.MAX_VALUE);
+        System.out.println("double_min = "+Double.MIN_VALUE);
+    }
+}
