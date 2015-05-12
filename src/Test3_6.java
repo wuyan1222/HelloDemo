595a0f36d1d726ba152cda1ebc33fdc47950bf66
WuYan <wuyan1222@126.com>
2015-05-12 13:34:22 +0800

    CharDemo

---
+++ src/Test3_6.java
@@ -0,0 +1,13 @@
+/**
+ * Created by yan on 2015/5/11.
+ */
+public class Test3_6 {
+    public static void main(String[] args) {
+        char ch1 = 97;
+        char ch2 = 'a';
+        System.out.println("ch1 = " + ch1);
+        System.out.println("ch2 = " + ch2);
+
+
+    }
+}
