393ed9dc99644126f0c2169667f6d15ef39c1bb1
WuYan <wuyan1222@126.com>
2015-05-12 13:46:22 +0800

    IntToFloatDemo

---
+++ src/Test3_11.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yan on 2015/5/12.
+ */
+public class Test3_11 {
+    public static void main(String[] args) {
+        int a = 156;
+        float b = 24.1f;
+        System.out.println("a="+a+" , b="+b);
+        System.out.println("a/b = "+(a/b));
+    }
+}
