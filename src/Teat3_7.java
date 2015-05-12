4378ee738762eaafc2bc14f9e677d3bc3b7d8122
WuYan <wuyan1222@126.com>
2015-05-12 13:38:22 +0800

    EscapeSequenceDemo

---
+++ src/Teat3_7.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yan on 2015/5/12.
+ */
+public class Teat3_7 {
+    public static void main(String[] args) {
+        char ch = '\'';
+        System.out.println(ch+"Test Escape Sequence!"+ch);
+        System.out.println("\"hello world!\"");
+
+    }
+}
