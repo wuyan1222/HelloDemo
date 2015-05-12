da802fce6bc4d73cc12e8e4013f9ea6aac9c0f79
WuYan <wuyan1222@126.com>
2015-05-12 14:11:05 +0800

    ExpressingLove

---
+++ src/ExpressingLove.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yan on 2015/5/12.
+ */
+public class ExpressingLove {
+    public static void main(String[] args) {
+        System.out.println("崔青，我爱你！"+"\n");
+        System.out.println("我会一直对你好的！");
+    }
+}
