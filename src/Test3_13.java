74b539129f4986c44d4ab38f0864ff385f900f61
WuYan <wuyan1222@126.com>
2015-05-12 14:17:19 +0800

    =Demo

---
+++ src/Test3_13.java
@@ -0,0 +1,13 @@
+/**
+ * Created by yan on 2015/5/12.
+ */
+//Use "=" assignment
+public class Test3_13 {
+    public static void main(String[] args) {
+        int num = 22;
+        System.out.println("First, num = "+num);
+        num = num -5;
+        System.out.println("In the end, num ="+num);
+
+    }
+}
