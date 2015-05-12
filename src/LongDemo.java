d9b2439e71bcea21133ac3cba62a3f8a130635d3
WuYan <wuyan1222@126.com>
2015-05-12 13:47:17 +0800

    LongDemo

---
+++ src/LongDemo.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yan on 2015/5/11.
+ */
+public class LongDemo {
+    public static void main(String args[]) {
+        long long_max = Long.MAX_VALUE;
+        System.out.println("Long Max Value = " + long_max);
+    }
+}
