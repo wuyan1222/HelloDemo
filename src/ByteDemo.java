9e8a7dd8adc6040641a87b057370c67a3f963569
WuYan <wuyan1222@126.com>
2015-05-12 13:47:45 +0800

    ByteDemo

---
+++ src/ByteDemo.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yan on 2015/5/9.
+ */
+public class ByteDemo {
+    public static void main(String args[]) {
+        byte byte_max = Byte.MAX_VALUE;
+        System.out.println("Byte Max Value = " + byte_max);
+    }
+}
