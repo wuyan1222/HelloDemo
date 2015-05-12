b11c80781bac08ac7ba5ac588d30e4450a71ed5a
WuYan <wuyan1222@126.com>
2015-05-12 13:42:03 +0800

    BooleanDemo

---
+++ src/Test3_10.java
@@ -0,0 +1,10 @@
+/**
+ * Created by yan on 2015/5/12.
+ */
+public class Test3_10 {
+    public static void main(String[] args) {
+        boolean status = true;
+        System.out.println("status = "+status);
+    }
+
+}
