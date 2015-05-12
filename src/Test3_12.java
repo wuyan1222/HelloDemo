c0d9577beb0babef2a88d0defde0efd02de826c8
WuYan <wuyan1222@126.com>
2015-05-12 13:54:28 +0800

    CoercionDemo

---
+++ src/Test3_12.java
@@ -0,0 +1,17 @@
+/**
+ * Created by yan on 2015/5/12.
+ */
+public class Test3_12 {
+    public static void main(String[] args) {
+        int a = 55;
+        int b =9;
+        float c,d;
+        System.out.println("a = "+a+" , b= "+b);
+        c =a/b;
+        System.out.println("a/b = "+c+"\n");
+        System.out.println("a = "+a+" , b= "+b);
+        d=(float)a/b;
+        System.out.println("a/b = "+d);
+
+    }
+}
