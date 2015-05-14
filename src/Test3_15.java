2ae62a8706382a90e4636339f14d893b6bd6aa7f
WuYan <wuyan1222@126.com>
2015-05-14 23:46:24 +0800

    SingleComputeDemo

---
+++ src/Test3_15.java
@@ -0,0 +1,12 @@
+/**
+ * Created by yan on 2015/5/14.
+ */
+public class Test3_15 {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 4;
+        System.out.println("a = "+a+" , b = "+b);
+        System.out.println("a/b = "+(a/b));
+        System.out.println("a/b = "+((float)a/b));
+    }
+}
