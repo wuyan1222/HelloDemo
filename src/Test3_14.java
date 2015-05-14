96ea67efc831b9a0aee18db939e569355386e522
WuYan <wuyan1222@126.com>
2015-05-14 23:43:53 +0800

    SingleComputeDemo

---
+++ src/Test3_14.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yan on 2015/5/14.
+ */
+public class Test3_14 {
+    public static void main(String[] args) {
+        byte a = Byte.MAX_VALUE;
+        boolean b = false;
+        System.out.println("a = "+a+" , ~a = "+(~a));
+        System.out.println("b = "+b+" , !b = ="+(!b));
+    }
+}
