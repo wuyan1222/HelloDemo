7677d962107f109d3aa526318e2647b9b62aa3a6
WuYan <wuyan1222@126.com>
2015-05-16 21:52:22 +0800

    IfDemo

---
+++ src/Test3_17.java
@@ -0,0 +1,13 @@
+/**
+ * Created by yan on 2015/5/16.
+ */
+public class Test3_17 {
+    public static void main(String[] args) {
+        if(5>2)
+            System.out.println("Return: "+(5>2));
+        if(true)
+            System.out.println("Hello Java !");
+        if((3+6)==(3-6))
+            System.out.println("I Like Java!");
+    }
+}
