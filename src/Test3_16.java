c9cdcedc406a7d9532d8a90dade63a5950a3c7ae
WuYan <wuyan1222@126.com>
2015-05-16 21:51:53 +0800

    %Demo

---
+++ src/Test3_16.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yan on 2015/5/16.
+ */
+public class Test3_16 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println(a+"%"+b+"="+(a%b));
+        System.out.println(b+"%"+a+"="+(b%a));
+    }
+}
