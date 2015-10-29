cf974436f103b1ac46e81be85380c99292fb42fb
wuyan1222 <wuyan1222@126.com>
2015-10-29 16:51:04 +0800

    Multiply

---
+++ src/Multiply.java
@@ -0,0 +1,12 @@
+
+public class Multiply {
+    public static void main(String[] args){
+        int i, j;
+        for(i=1;i<10;i++){
+            for(j=1;j<i;j++){
+                System.out.print(i+"*"+j+"="+i*j+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
